dc36db27c2fffee49db7497855701c5dd527c3d9
Jordan Jennings <jordanjennings12@gmail.com>
2022-08-13 08:20:44 -0400

    commit tree a6b687888006b858525e66186fea1728eb753357

--- main.js
+++ main.js
@@ -3,7 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("#modal")
+modal.className = "hidden"
 
+
+
+  const hearts = document.querySelectorAll(".like-glyph")
+  
+  hearts.forEach(heart => {
+    heart.addEventListener("click", addLike)
+  })
+ // heart.addEventListener("click", addLike)
+
+  function addLike(e){
+    // const servResponse = mimicServerCall("url").then()
+    // console.log(servResponse)
+    mimicServerCall()
+    .then(() =>
+    {
+      if(e.target.innerHTML === EMPTY_HEART){
+        e.target.textContent = FULL_HEART
+        e.target.className = "activated-heart"
+      } else {
+        e.target.textContent = EMPTY_HEART
+        e.target.className = ""
+      }
+    })
+    .catch((err) => {
+      modal.className = "";
+      modal.childNodes[1].textContent =  err;
+      setTimeout(() => modal.className = "hidden", 3000);
+    })
+   
+  }
 
 
 
